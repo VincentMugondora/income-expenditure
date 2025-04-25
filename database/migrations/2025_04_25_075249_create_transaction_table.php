b5aa94f615349f5da36e07d5d76b52546c3bc32d
vincentmugondora <vinmugondora@gmail.com>
2025-04-25 10:08:24 +0200

    first commit

---
+++ database/migrations/2025_04_25_075249_create_transaction_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->date('transaction_date');
+            $table->string('description')->nullable();
+            $table->decimal('debit', 15, 2)->default(0.00);
+            $table->decimal('credit', 15, 2)->default(0.00);
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->unsignedBigInteger('subcategory_id')->nullable();
+            $table->decimal('commission', 15, 2)->default(0.00);
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
+            $table->foreign('subcategory_id')->references('id')->on('subcategories')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
