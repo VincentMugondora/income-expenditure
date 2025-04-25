0d0b209f657a8a644c0ce0f249318831975c5679
vincentmugondora <vinmugondora@gmail.com>
2025-04-25 11:29:23 +0200

    Add LogController with initial structure

--- app/Models/SubCategory.php
+++ app/Models/SubCategory.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SubCategory extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['category_id', 'name'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
